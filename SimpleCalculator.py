842f9b6c506a864d7e28edc59ae1e9e9b91ac461
Kannappan Sriramajayam <you@example.com>
2022-06-19 23:15:00 -0400

    Python Program for Simple Calculator

---
+++ SimpleCalculator.py
@@ -0,0 +1,35 @@
+#Program to make a simple calculator without define function
+
+#take inputs
+
+n1 = float(input("Enter first number: "))
+
+n2 = float(input("Enter second number: "))
+
+#choice of operation
+
+print("Operations: +, -, *, /")
+
+choice = input("Select Operations: ")
+
+#check operation and display result
+
+if choice == '+':
+    
+    print(n1, '+', n2, '=', n1 + n2)
+
+elif choice == '-':
+    
+    print(n1, '-', n2, '=', n1 - n2)
+
+elif choice == '*':
+    
+    print(n1, '*', n2, '=', n1 * n2)
+
+elif choice == '/':
+    
+    print(n1, '/', n2, '=', n1 / n2)
+
+else:
+    
+    print("Invalid Input")
